985b4867754b8b1ce1b21737753709a931876d2b
toxicaker <toxicaker@liangjiateng.cn>
2018-05-29 09:02:44 +0800

    fix: configuration

--- src/main/java/cn/liangjiateng/App.java
+++ src/main/java/cn/liangjiateng/App.java
@@ -1,5 +1,6 @@
 package cn.liangjiateng;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Jiateng on 5/28/18.
  */
 @SpringBootApplication
+@MapperScan("cn.liangjiateng.mapper")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
---
+++ src/main/java/cn/liangjiateng/common/ErrorCode.java
@@ -0,0 +1,7 @@
+package cn.liangjiateng.common;
+
+/**
+ * Created by Jiateng on 5/28/18.
+ */
+public enum ErrorCode {
+}
