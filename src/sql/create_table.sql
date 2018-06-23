9ed6e02e918767ccd45129dc899baa9bd0230542
toxicaker <toxicaker@liangjiateng.cn>
2018-06-23 19:38:26 +0800

    add: job table

--- src/sql/create_table.sql
+++ src/sql/create_table.sql
@@ -57,3 +57,19 @@ create table doc_template
   comment '文档模板表'
   engine = InnoDB default charset=utf8;
 
+CREATE TABLE job
+(
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(50) NOT NULL COMMENT '任务名称',
+    job_id varchar(50) NOT NULL COMMENT '程序指定的任务id，唯一',
+    status int DEFAULT 0 NOT NULL COMMENT '-1删除，0停止，1执行中，2暂停',
+    executed_times bigint DEFAULT 0 NOT NULL COMMENT '执行次数',
+    cron varchar(255) DEFAULT '' COMMENT '执行策略',
+    start_date timestamp DEFAULT '2000-01-01 00:00:00' COMMENT '开始时间，空则立刻开始',
+    end_date timestamp DEFAULT '2000-01-01 00:00:00' COMMENT '结束时间，空则一直运行',
+    create_time timestamp DEFAULT current_timestamp NOT NULL,
+    update_time timestamp DEFAULT current_timestamp NOT NULL
+)
+  comment '定时任务'
+  engine = InnoDB default charset=utf8;;
+CREATE UNIQUE INDEX job_job_id_uindex ON job (job_id);
