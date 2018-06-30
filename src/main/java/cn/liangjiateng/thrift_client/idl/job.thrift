72ad65572c065e8ac6a732523208f58b875a473e
toxicaker <toxicaker@liangjiateng.cn>
2018-06-30 10:40:57 +0800

    add: rpc status()

--- src/main/java/cn/liangjiateng/thrift_client/idl/job.thrift
+++ src/main/java/cn/liangjiateng/thrift_client/idl/job.thrift
@@ -1,27 +1,30 @@
-exception cn.liangjiateng.thrift_client.job.JobServiceException{
+exception JobServiceException{
     1: i32 code,
     2: string msg,
     3: string time,
     4: string detail
 }
 
-service cn.liangjiateng.thrift_client.job.JobRPCService{
+service JobRPCService{
 
-    void start_scheduler() throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void start_scheduler() throws (1: JobServiceException ex)
 
-    void stop_scheduler() throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void stop_scheduler() throws (1: JobServiceException ex)
 
-    void pause_scheduler() throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void pause_scheduler() throws (1: JobServiceException ex)
 
-    void resume_scheduler() throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void resume_scheduler() throws (1: JobServiceException ex)
 
-    void start_job(1: string job_id) throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void start_job(1: string job_id) throws (1: JobServiceException ex)
 
-    void stop_job(1: string job_id) throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void stop_job(1: string job_id) throws (1: JobServiceException ex)
 
-    void pause_job(1: string job_id) throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void pause_job(1: string job_id) throws (1: JobServiceException ex)
 
-    void modify_job(1: string job_id, 2: string config) throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    void modify_job(1: string job_id, 2: string config) throws (1: JobServiceException ex)
 
-    string submit_job(1: string file_bytes, 2: string config) throws (1: cn.liangjiateng.thrift_client.job.JobServiceException ex)
+    string submit_job(1: string file_bytes, 2: string config) throws (1: JobServiceException ex)
+
+    # 0 停止，1 运行，2暂停
+    i32 status() throws (1: JobServiceException ex)
 }
